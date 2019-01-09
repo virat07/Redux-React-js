5dafa063e4b8903563e30ff288a8074d736870ea
virat07 <abond0707@gmail.com>
2019-01-09 11:05:22 +0530

    CLEAR-1111: Updated design of components

--- src/container/Leftside.js
+++ src/container/Leftside.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 export default class Leftside extends Component {
     render() {
         return (
--- src/container/RightSide.js
+++ src/container/RightSide.js
@@ -1,43 +1,27 @@
 import React, { Component } from 'react';
 import './Rightside.css';
-class RightSide extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let Detailed = this.props.updates
-
-        return (
-            <div className='parent-container'>
-
-                <div className='right-container'>
-
-                    <div className="column_1" id="data">
-                        <h2>Name</h2>
-                    </div>
-                    <div className="column_1" id="data">
-                        <h2>Description</h2>
-                    </div>
-                    <div className="column_1" id="data">
-                        <h2>Category</h2>
-                    </div>
-
-
-
-                    {Detailed.map(Detailed => <div className='row' key={Detailed.name}>
-                        <div className='column'>{Detailed.name}</div>
-                        <div className='column'>{Detailed.description}</div>
-                        <div className='column'>{Detailed.category} </div>
-                    </div>
-                    )}
+function RightSide(props) {
+    let Detailed = props.updates;
+    return (
+        <div className='parent-container'>
+            <div className='right-container'>
+                <div className="column_1" id="data">
+                    <h2>Name</h2>
                 </div>
-
-
-
+                <div className="column_1" id="data">
+                    <h2>Description</h2>
+                </div>
+                <div className="column_1" id="data">
+                    <h2>Category</h2>
+              </div>
+                {Detailed.map(Detailed => <div className='row' key={Detailed.name}>
+                    <div className='column'>{Detailed.name}</div>
+                    <div className='column'>{Detailed.description}</div>
+                    <div className='column'>{Detailed.category} </div>
+                </div>
+                )}
             </div>
-
-
-        )
-    }
+        </div>
+    )
 }
 export default RightSide;
